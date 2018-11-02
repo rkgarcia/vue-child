f4b583a10af33b5dccce1c001a3c98a7a00ce19c
Roberto Carlos García Luis <rgarcia@kairos.systems>
2018-11-02 15:21:33 -0600

    Adding the basics

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <child :selected="selected" name="main" @change="select"/>
+    <child :selected="selected" name="second" @change="select"/>
+    <child :selected="selected" name="other" @change="select"/>
+    <child v-for="item in menus" :key="item" :selected="selected" :name="item" @change="select"/>
+  </div>
+</template>
+
+<script>
+import Child from './components/Child'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      selected: 'main',
+      menus: [
+        'other1',
+        'other2',
+        'other3',
+        'othern'
+      ]
+    }
+  },
+  methods: {
+    select (newSelection) {
+      this.selected = newSelection
+    }
+  },
+  components: {
+    Child
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="select" :class="state" class="option">
+    {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    state () {
+      return this.name === this.selected ? 'active' : 'inactive'
+    }
+  },
+  methods: {
+    select () {
+      this.$emit('change', this.name)
+    }
+  },
+  props: ['name', 'selected']
+}
+</script>
+
+<style scoped>
+.option {
+  padding: 1rem;
+}
+.active {
+  background-color: red;
+  color: white;
+}
+</style>
